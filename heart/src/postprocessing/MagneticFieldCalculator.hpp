58faf80b73ef8c162fe7b836e883b1898cffb68b
Jouni <jouni.m.karjalainen@gmail.com>
2022-08-24 13:46:33 +0300

    First version of the magnetic field calculator. Not tested/verified inside the conducting media and probably will not work there.

--- heart/src/postprocessing/MagneticFieldCalculator.hpp
+++ heart/src/postprocessing/MagneticFieldCalculator.hpp
@@ -4,13 +4,31 @@
 #include "AbstractFunctionalCalculator.hpp"
 #include "ChastePoint.hpp"
 #include "AbstractTetrahedralMesh.hpp"
+#include "Hdf5DataReader.hpp"
 
+/**
+ * Calculates the magnetic field in cT (10^-2 T) from electric potential
+ * quasistatically, using Ohm's law and Biot-Savart-law.
+ * 
+ * @tparam ELEMENT_DIM 
+ * @tparam SPACE_DIM   Only 3D is currently supported.
+ * @tparam PROBLEM_DIM Number of electrical potentials in the solution.
+ */
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
 class MagneticFieldCalculator : public AbstractFunctionalCalculator<ELEMENT_DIM, SPACE_DIM, PROBLEM_DIM>
 {
 private:
 
     friend class TestMagneticFieldCalculator;
+
+    Hdf5DataReader* mpDataReader; /**< An HDF5 reader from which to get the solution*/
+    unsigned mNumberOfNodes; /**< Number of nodes in the mesh (got from the data reader)*/
+    unsigned mNumTimeSteps;/**< Number of time steps in the simulation (got from the data reader)*/
+    AbstractTetrahedralMesh<ELEMENT_DIM,SPACE_DIM>& mrMesh;/**< A mesh used by the calculator*/
+    ChastePoint<SPACE_DIM> mObservationPoint;
+    std::string mVariableName;/**< the variable for which we want to calculate the pseudo ecg, defaults to "V"*/
+    unsigned mTimestepStride; /**< The number of timesteps in a stride (so that we don't have to compute all the ECGs).  This defaults to 1.*/
+    double mConductivity; /**< The conductivity of the domain (mS/cm)*/
 
     /**
      * @return the integrand
@@ -29,6 +47,16 @@ private:
 public:
 
     /**
+     * Calculates the magnetic field and returns its value at the given time step.
+     *
+     * @param timeStep the time step where we want to calculate the magnetic field
+     * @return the magnetic field at the given time step.
+     *
+     */
+    double ComputeMagneticFieldAtOneTimeStep (unsigned timeStep);
+
+
+    /**
      * The parameters just copied from PseudoEcgCalculator. 
      * 
      */
@@ -36,7 +64,7 @@ public:
                             const ChastePoint<SPACE_DIM>& rObservationPoint,
                             const FileFinder& rDirectory,
                             const std::string& rHdf5FileName,
-                            const std::string& rVariableName = "B",
+                            const std::string& rVariableName = "V",
                             unsigned timestepStride = 1);
 
     /**
@@ -44,5 +72,18 @@ public:
      */
     ~MagneticFieldCalculator();
 };
+
+
+/**
+ * Calculates the cross product (u x v) of two 3D-vectors u and v.
+ * 
+ * @param u the first vector
+ * @param v the second vector
+ * @return c_vector<double, 3> 
+ */
+c_vector<double, 3> CrossProduct(const c_vector<double, 3>& u, const c_vector<double, 3>& v);
+
+
+
 
 #endif /* _MAGNETICFIELDCALCULATOR_ */
--- heart/test/postprocessing/TestMagneticFieldCalculator.hpp
+++ heart/test/postprocessing/TestMagneticFieldCalculator.hpp
@@ -2,15 +2,112 @@
 #define _TESTMAGNETICFIELDCALCULATOR_HPP_
 
 #include "MagneticFieldCalculator.hpp"
+#include "TrianglesMeshReader.hpp"
+#include "DistributedTetrahedralMesh.hpp"
+#include "PetscSetupAndFinalize.hpp"
+#include "Hdf5DataWriter.hpp"
+#include "PetscVecTools.hpp"
 
 class TestMagneticFieldCalculator : public CxxTest::TestSuite
 {
 
 public:
 
-    void TestThatWillFail()
+
+    /**
+     * Create a linear gradient of electric potential along the z-direction
+     * in a cube. Compute magnetic field outside the cube. 
+     * 
+     */
+    void TestCubic()
     {
-        TS_ASSERT(1==0);
+        // Read cubic mesh with 2mm sides
+        TrianglesMeshReader<3,3> reader("mesh/test/data/cube_2mm_152_elements");
+        DistributedTetrahedralMesh<3,3> mesh;
+        mesh.ConstructFromMeshReader(reader);
+        // Correct result is expressed in cT (10^-2 T), which is the unit of
+        // magnetic field flux density in Chaste units
+
+        const double correct_magnetic_field_x=2.47533e-8;  
+        unsigned number_nodes = mesh.GetNumNodes();
+
+        DistributedVectorFactory factory(number_nodes);
+        Hdf5DataWriter writer(factory, "hdf5", "linear_V", false);
+        writer.DefineFixedDimension(number_nodes);
+        
+        int node_id = writer.DefineVariable("Node", "dimensionless");
+        int V_id = writer.DefineVariable("V", "mV");
+        
+        writer.DefineUnlimitedDimension("Time", "msec");
+        writer.EndDefineMode();
+        
+        
+        Vec petsc_data_1 = factory.CreateVec();
+        DistributedVector distributed_vector_1 = factory.CreateDistributedVector(petsc_data_1);
+
+        Vec petsc_data_2 = factory.CreateVec();
+        DistributedVector distributed_vector_2 = factory.CreateDistributedVector(petsc_data_2);
+
+
+        // Create a result with an electric potential that has a linear
+        // gradient along the z-axis from V = 10 mV to V = -10 mV. Chaste
+        // uses mV for voltage and cm for length, so 
+        //
+        // k = (-10mV - 10mV)/(0.2cm - 0cm) = -100mV/cm
+        // V = k*z -10mV
+        //
+        const double k = -100;
+        unsigned time_step = 0;
+
+        for (DistributedTetrahedralMesh<3,3>::NodeIterator node_iter = mesh.GetNodeIteratorBegin();
+             node_iter != mesh.GetNodeIteratorEnd();
+             ++node_iter)
+        {
+            distributed_vector_1[node_iter -> GetIndex()] = node_iter -> GetIndex(); 
+            c_vector<double, 3> pos = node_iter -> rGetLocation();
+            distributed_vector_2[node_iter -> GetIndex()] = pos[2] * k - 10;
+        }
+        distributed_vector_1.Restore();
+        distributed_vector_2.Restore();
+    
+        writer.PutVector(node_id, petsc_data_1);
+        writer.PutVector(V_id, petsc_data_2);
+        writer.PutUnlimitedVariable(time_step);
+        writer.AdvanceAlongUnlimitedDimension();
+        writer.Close();
+
+        PetscTools::Destroy(petsc_data_1);
+        PetscTools::Destroy(petsc_data_2);
+
+        // Compute the magnetic field at x = 1 mm, y = 0, z = -2 mm.
+        ChastePoint<3> reference_point(0.1, 0, -0.2);
+        MagneticFieldCalculator<3,3,1> calculator(mesh, reference_point,
+                                       FileFinder("hdf5", RelativeTo::ChasteTestOutput),
+                                       "linear_V", "V");
+        double magnetic_field_x = calculator.ComputeMagneticFieldAtOneTimeStep(0);
+
+        printf("%g", magnetic_field_x);
+        TS_ASSERT_DELTA(magnetic_field_x, correct_magnetic_field_x, 1e-12);
+    }
+
+    void TestCrossProduct()
+    {
+        c_vector<double, 3> x_vec, y_vec, z_vec;
+        x_vec[0] = 1.; x_vec[1] = 0.; x_vec[2] = 0.;
+        y_vec[0] = 0.; y_vec[1] = 2.; y_vec[2] = 0.;
+        z_vec[0] = 0.; z_vec[1] = 0.; z_vec[2] = 3.;
+        c_vector<double, 3> result = CrossProduct(x_vec, y_vec);
+        TS_ASSERT_DELTA(result[0], 0., 1e-6);
+        TS_ASSERT_DELTA(result[1], 0., 1e-6);
+        TS_ASSERT_DELTA(result[2], 2., 1e-6);
+        result = CrossProduct(z_vec, y_vec);
+        TS_ASSERT_DELTA(result[0], -6, 1e-6);
+        TS_ASSERT_DELTA(result[1], 0, 1e-6);
+        TS_ASSERT_DELTA(result[2], 0, 1e-6);
+        result = CrossProduct(x_vec, z_vec);
+        TS_ASSERT_DELTA(result[0], 0, 1e-6);
+        TS_ASSERT_DELTA(result[1], -3, 1e-6);
+        TS_ASSERT_DELTA(result[2], 0, 1e-6); 
     }
 };
 
